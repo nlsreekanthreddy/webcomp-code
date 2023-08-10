a5c29be433576915569a00dd2bba22aa56b381dc
Sreekanth reddy <nlsreekanthreddy@gmail.com>
2023-08-10 10:04:24 +0530

    custom js modified

--- TodoItem.js
+++ TodoItem.js
@@ -22,23 +22,19 @@ class TodoItem extends HTMLElement{
             <slot name= "description"></slot>
             </span>
             </label>
-            <h3> custom ele text</h3?
-            
+      
 
             `
-// slot is a placeholder for content of html element 
-// with web components , can do named slots 
 
         const shadow = this.attachShadow({mode:"open"}) //open to make modifications to shadow dom with this.shadowRoot 
-        // this.innerHTML = "HTML custom element"
-        // this.innerHTML = ` <h3>${this.innerText}</h3>` h3 style applied from index.html
-        // this.innerHTML = ` <style >h3{color: green} </style><h3>${this.innerText}</h3>`
-        //style applied to all h3 elements in dom    *to avoid this one should use shadow DOM
-  
         shadow.append(template.content.cloneNode(true))
  
         // this.title.innerText =  this.innerText
 }
 }
 
+
 customElements.define("todo-item", TodoItem );
+export {
+    TodoItem
+};
